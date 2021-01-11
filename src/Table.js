ebb8ebb0301427047ece81da7b7a03c9ab27c6b9
Parth Ray <parthray16@gmail.com>
2021-01-11 10:13:50 -0800

    table->functional,added removeCharacter

--- src/Table.js
+++ src/Table.js
@@ -1,38 +1,37 @@
-import React, { Component } from 'react'
-
-const TableHeader = () => {
-    return (
-      <thead>
-        <tr>
-          <th>Name</th>
-          <th>Job</th>
-        </tr>
-      </thead>
-    )
-  }
-const TableBody = props => {
-    const rows = props.characterData.map((row, index) => {
+import React from 'react'
+  const TableHeader = () => {
       return (
-        <tr key={index}>
-          <td>{row.name}</td>
-          <td>{row.job}</td>
-        </tr>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Job</th>
+          </tr>
+        </thead>
       )
-    })
-  
-    return <tbody>{rows}</tbody>
-}  
-class Table extends Component {
+    }
+  const TableBody = props => {
+      const rows = props.characterData.map((row, index) => {
+        return (
+          <tr key={index}>
+            <td>{row.name}</td>
+            <td>{row.job}</td>
+            <td>
+              <button onClick={() => props.removeCharacter(index)}>Delete</button>
+            </td>
+          </tr>
+        )
+      })
     
-  render() {
-    const {characterData} = this.props
+      return <tbody>{rows}</tbody>
+  }
+  const Table = props => {
+    const { characterData, removeCharacter } = props
+  
     return (
       <table>
-          <TableHeader />
-          <TableBody characterData={characterData}/>
+        <TableHeader />
+        <TableBody characterData={characterData} removeCharacter={removeCharacter} />
       </table>
     )
   }
-}
-
 export default Table
