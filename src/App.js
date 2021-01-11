120c7dd16233ed6cc477e86c48bab49a685a69e2
Parth Ray <parthray16@gmail.com>
2021-01-11 10:26:23 -0800

    Form created and implemented

--- src/App.js
+++ src/App.js
@@ -1,30 +1,27 @@
 import React, { Component } from 'react'
 import Table from './Table'
+import Form from './Form';
 class App extends Component {
-  render() {
-    const characters = [
-      {
-        name: 'Charlie',
-        job: 'Janitor',
-      },
-      {
-        name: 'Mac',
-        job: 'Bouncer',
-      },
-      {
-        name: 'Dee',
-        job: 'Aspring actress',
-      },
-      {
-        name: 'Dennis',
-        job: 'Bartender',
-      },
-    ]
+    state = { characters : [] }  
+
+    removeCharacter = index => {
+      const { characters } = this.state
+    
+      this.setState({
+        characters: characters.filter((character, i) => {
+          return i !== index
+        }),
+      })
+    }
+
+    render() {
+      const { characters } = this.state;
     return (
       <div className="container">
-        <Table characterData={characters}/>
+        <Table characterData={characters} removeCharacter={this.removeCharacter} />
+        <Form />
       </div>
-    )
+	);
   }
 }
 
